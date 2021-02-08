4477cdb49a2896cd3dfc380a7c2fc133164ef015
yangxinlong <loongyufei@163.com>
2021-02-08 18:56:02 +0800

    pref: 添加中国身份证号码

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,3 +2,4 @@ export declare const number: RegExp;
 export declare const mobile_zh: RegExp;
 export declare const phone_zh: RegExp;
 export declare const idCard_zh: RegExp;
+export declare const number_n: (n: number) => RegExp;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 'use strict';
 const expect = require('chai').expect;
-const { number, idCard_zh } = require('../dist/index')
+const { number, idCard_zh,number_n } = require('../dist/index')
 
 describe('regexp-template number test', () => {
   it('should return true', () => {
@@ -33,6 +33,20 @@ describe('regexp-template idCard_zh test', () => {
 describe('regexp-template idCard_zh test', () => {
   it('should return false', () => {
     const result = idCard_zh.test("1302241994031362121")
+    expect(result).to.equal(false);
+  });
+});
+
+describe('regexp-template number_n test', () => {
+  it('should return true', () => {
+    const result = number_n(3).test("123")
+    expect(result).to.equal(true);
+  });
+});
+
+describe('regexp-template number_n test', () => {
+  it('should return false', () => {
+    const result = number_n(3).test("1233")
     expect(result).to.equal(false);
   });
 });
